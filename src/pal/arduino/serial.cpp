a22261b41c09d6aff8e41b4b6e932cca62e4a56b
Steven Giacomelli <stevegiacomelli@gmail.com>
2021-09-30 15:56:42 -0400

    Create serial.cpp
    serial printing platform Arduino abstraction

---
+++ src/pal/arduino/serial.cpp
@@ -0,0 +1,25 @@
+#if defined ARDUINO_PLATFORM
+#include <Arduino.h>
+#include <string>
+
+namespace xiaxr {
+namespace detail {
+namespace {
+static bool serial_init = false;
+
+void initialize_serial() {
+  if (!serial_init) {
+    Serial.begin(74880);
+    serial_init = true;
+  }
+}
+
+}  // namespace
+
+auto serial_print_line(const std::string& value) -> size_t {
+  return Serial.println(value.c_str());
+}
+}  // namespace detail
+}  // namespace xiaxr
+
+#endif
